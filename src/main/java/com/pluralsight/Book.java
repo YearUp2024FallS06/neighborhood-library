27ae7f919a5d9aa367bbcd68d3b516afd5511159
mattchristenson <matt@rcc.team>
2024-10-07 09:27:26 -0500

    populate library

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -6,6 +6,55 @@ public class Book {
     private String isbn;
     private String title;
     private boolean isCheckedOut;
-    private String checkoutOut;
+    private String checkedOutTo;
 
+    public Book( int id, String title, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void checkOut(String name){
+        this.checkedOutTo = name;
+        this.isCheckedOut = true;
+    }
+
+    public void checkIn(){
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", isCheckedOut=" + isCheckedOut +
+                ", checkedOutTo='" + checkedOutTo + '\'' +
+                '}';
+    }
 }
